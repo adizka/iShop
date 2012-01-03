e7770f2c1e19328d70f6864d6ad3d9483c51cb7c
Ruslan Ibragimov <github@mail.ru>
2012-01-03 15:24:08 +0500

    Scripts Updated

--- DBScripts/GetProductDataByCategoryName.sql
+++ DBScripts/GetProductDataByCategoryName.sql
@@ -1,10 +1,8 @@
 USE [MarvelDB]
 GO
-
-/****** Object:  UserDefinedFunction [dbo].[GetProductDataByCategoryName]    Script Date: 12/26/2011 16:07:29 ******/
+/****** Object:  UserDefinedFunction [dbo].[GetProductDataByCategoryName]    Script Date: 01/03/2012 15:22:28 ******/
 SET ANSI_NULLS ON
 GO
-
 SET QUOTED_IDENTIFIER ON
 GO
 
@@ -13,15 +11,12 @@ ALTER FUNCTION [dbo].[GetProductDataByCategoryName](
 	@SearchCriteria nvarchar(200))
 RETURNS TABLE
 as
-return		select DISTINCT prod.* , prodProp.[PropertyValue]
+return		
+		select prod.* , prodProp.[PropertyValue],cat.[CategoryID], cat.[Name] as CategoryName
 			from [dbo].[ProductsRefCategories] ref
 				inner join [dbo].[Products] prod on (prod.ProductID = ref.ProductID)
 				inner join [dbo].[ProductProperties] prodProp on (prodProp.ProductID = prod.ProductID)
 				inner join [dbo].[Categories] cat on (cat.CategoryID = ref.CategoryID)
 				where
-					cat.Name like (N'%' +LTRIM(RTRIM(@SearchCriteria)) + N'%')
-					and  prodProp.[PropertyName] = 'ProductPhotoPreview'
-
-GO
-
-
+					(lower(cat.Name) like lower(N'%' +LTRIM(RTRIM(@SearchCriteria)) + N'%')
+					and  prodProp.[PropertyName] = 'ProductPhotoPreview')
--- DBScripts/GetProductDataByProductName.sql
+++ DBScripts/GetProductDataByProductName.sql
@@ -1,16 +1,22 @@
+USE [MarvelDB]
+GO
+/****** Object:  UserDefinedFunction [dbo].[GetProductDataByProductName]    Script Date: 01/03/2012 15:23:22 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
 
 ALTER FUNCTION [dbo].[GetProductDataByProductName](
 	@SearchCriteria nvarchar(200))
 RETURNS TABLE
 as
-return select DISTINCT prod.* , prodProp.[PropertyValue]
+return select  prod.* , prodProp.[PropertyValue], cat.[CategoryID], cat.[Name] as CategoryName
 			from [dbo].[ProductsRefCategories] ref
 				inner join [dbo].[Products] prod on (prod.ProductID = ref.ProductID)
 				inner join [dbo].[ProductProperties] prodProp on (prodProp.ProductID = prod.ProductID)
 				inner join [dbo].[Categories] cat on (cat.CategoryID = ref.CategoryID)
 				where
-					 prod.Name like (N'%' +LTRIM(RTRIM(@SearchCriteria)) + N'%') and
+					 lower(prod.Name) like lower(N'%' +LTRIM(RTRIM(@SearchCriteria)) + N'%') and
 					 prodProp.[PropertyName] = 'ProductPhotoPreview'
 					 
 			
-go
